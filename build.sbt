d6d8cbfd229e61e60f7acc60a56b20cf2bb10495
wardziniak <bwardziniak@yahoo.pl>
2018-04-19 14:32:50 +0200

    Starting implementation of CloneField and currentTimestamp transforms

--- build.sbt
+++ build.sbt
@@ -3,4 +3,11 @@ name := "kafka-war-transform"
 version := "1.0"
 
 scalaVersion := "2.12.1"
-        
+
+resolvers += "Confluent" at "http://packages.confluent.io/maven/"
+
+libraryDependencies ++= Seq(
+  Dependencies.kafkaConnectTransforms,
+  Dependencies.kafkaConnectApi,
+  Dependencies.confluentJdbcConnector
+)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,16 @@
+import sbt.ModuleID
+import sbt._
+
 /**
   * Created by wardziniak on 18.04.2018.
   */
 object Dependencies {
+
+  val kafkaConnectApi: ModuleID = "org.apache.kafka" % "connect-api" % "1.0.0"
+
+  val kafkaConnectTransforms: ModuleID = "org.apache.kafka" % "connect-transforms" % "1.0.0"
+
+  val confluentJdbcConnector: ModuleID = "io.confluent" % "kafka-connect-jdbc" % "4.0.0"
+
 
 }
